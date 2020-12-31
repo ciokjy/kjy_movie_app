54544993e416091562811b8fe2c0a851b029e811
ciokjy <66233808+ciokjy@users.noreply.github.com>
2020-12-31 10:28:42 +0900

    Updates

---
+++ precache-manifest.ef2b07ccb4fd5678a11d7a43735f9fc9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fbb096c008a3350500b99c2ae33f14e",
+    "url": "/kjy_movie_app/index.html"
+  },
+  {
+    "revision": "f13121553d7a7248b2af",
+    "url": "/kjy_movie_app/static/css/main.672a5149.chunk.css"
+  },
+  {
+    "revision": "3292616cc5c511783277",
+    "url": "/kjy_movie_app/static/js/2.558ee272.chunk.js"
+  },
+  {
+    "revision": "f13121553d7a7248b2af",
+    "url": "/kjy_movie_app/static/js/main.cae0bfb0.chunk.js"
+  },
+  {
+    "revision": "c2ca57929ad3e952dbba",
+    "url": "/kjy_movie_app/static/js/runtime~main.e66ab738.js"
+  }
+]);
